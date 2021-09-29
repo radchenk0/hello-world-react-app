08b2c4af1364ea51877c2f82041c41343cc8560f
Mikhail Radchenko <mradchenko@provenworks.com>
2021-09-29 18:45:46 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React!!!
         </a>
       </header>
       <p id={`${isProduction}_id`}>{isProduction ? 'production' : 'staging'}</p>
