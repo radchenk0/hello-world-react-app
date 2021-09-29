587ce33e4035a547821a04214e4d91bf2f4f4692
Mikhail Radchenko <mradchenko@provenworks.com>
2021-09-29 11:52:11 +0300

    example flow

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('checks REACT_APP_NODE_ENV value', () => {
+  const nodeEnv = process.env.REACT_APP_NODE_ENV;
+  console.log(nodeEnv);
+  expect(nodeEnv).toEqual('staging');
+});
